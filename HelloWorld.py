75552ab92795894cae0f69431b8eabaa04ec4037
j03Lsc <jsarmiento@interamerican.edu.ec>
2015-05-04 08:54:23 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print "Hello World"
+print "My name is Joel"
+print "I am 33 years old \nThis is Python"
+
+print 2015 + 1 #Printing an integer
+print "2015"
+
+print 3.1416 #Printing a floating point number
+
+#Math Operations
+print 1+1
+print 1-1
+print 1*1
+print 1/1
+
+#Modulus
+print 10%3
+
+print "Odd or Even Numbers"
+print 17 % 2
+print 16 % 2
